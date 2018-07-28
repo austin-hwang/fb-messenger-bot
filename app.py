5d2074dea14fcb2d64b5b53f039bb50230db3e5a
achou11 <chouandrewo@gmail.com>
2018-07-28 16:29:56 -0400

    Fix

--- app.py
+++ app.py
@@ -66,12 +66,12 @@ def webhook():
                     if message_text.lower() == 'subscribe':
                         with open('./db.txt', 'ra') as database:
                             users = database.readlines()
-                            if not any(sender_id in u for u.strip() in users):
+                            if not any(sender_id in u.strip() for u in users):
                                 database.write(sender_id + '\n')
                     elif message_text.lower() == 'unsubscribe':
                         with open('./db.txt', 'ra') as database:
                             users = database.readlines()
-                            if any(sender_id in u for u.strip() in users):
+                            if any(sender_id in u.strip() for u in users):
                                 index = users.index(sender_id)
                                 del users[index]
                                 for id in users:
